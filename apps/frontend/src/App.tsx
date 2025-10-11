34f2d4b9cb6c4396650b2327a55c57e3fd58071c
yuki.kitazawa <yuki.kitazawa.je@apresiasystems.co.jp>
2025-10-11 13:41:00 +0900

    refactor: 動作確認用の仮実装を削除 #61

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,13 +1,6 @@
 import { VoiceChatPage } from "./pages/VoiceChatPage";
-import { WhisperTestPage } from "./pages/WhisperTestPage";
 
 function App() {
-  const currentPath = window.location.pathname;
-
-  if (currentPath === "/whisper-test") {
-    return <WhisperTestPage />;
-  }
-
   return <VoiceChatPage />;
 }
 
